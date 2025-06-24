6a187eba4aa68b2b264d29760ccf3d51000924c4
alendavies <alendavies10@gmail.com>
2025-06-24 17:40:03 -0300

    Add ControlField and frame types for HDLC protocol

--- telemetry-protocol/src/frame.rs
+++ telemetry-protocol/src/frame.rs
@@ -1,3 +1,5 @@
+use crate::control::ControlField;
+
 /*
  * Flag | Address | Control | Payload | FCS | Flag
  * 0x7E |   1B    |   1B    |  nB     |  2B | 0x7E
@@ -5,7 +7,7 @@
 
 pub struct HdlcFrame {
     address: u8,
-    control: u8,
+    control: ControlField,
     payload: Vec<u8>,
     fcs: u16,
 }
