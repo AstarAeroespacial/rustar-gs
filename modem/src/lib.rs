5e43b2e6ea605548f3d584e5cf893aa2ef99033a
Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>
2025-08-20 20:31:19 -0300

    chore: implement modem traits
    Co-authored-by: Alen Davies <alendavies@users.noreply.github.com>

--- modem/src/lib.rs
+++ modem/src/lib.rs
@@ -1,5 +1,13 @@
+pub mod afsk1200;
 pub mod demodulator;
 pub mod modulator;
 
-trait BitSink {}
-trait SampleSource {}
+/// `S`: type of the samples input.
+///
+/// `B`: type of the returned bits.
+pub trait Demodulator<S, B> {
+    type Input: Iterator<Item = S>;
+    type Output: Iterator<Item = B>;
+
+    fn bits(&self, input: Self::Input) -> Self::Output;
+}
