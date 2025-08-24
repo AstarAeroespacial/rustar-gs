c0e98b6eb0c5f542eda9cbe11bc4036314cdf042
Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>
2025-08-24 16:56:54 -0300

    Integration with mocked time, demod, deframer & observations iterator

--- ground-station/src/main.rs
+++ ground-station/src/main.rs
@@ -1,8 +1,6 @@
-use antenna_controller::{self, AntennaController, SerialAntennaController};
-use chrono::Utc;
-use framing::deframe::Deframer;
-use framing::hdlc::HdlcDeframer;
-use modem::{Demodulator, afsk1200::AfskDemodulator};
+use antenna_controller::{self, AntennaController, mock::MockController};
+use demod::{Demodulator, example::ExampleDemod};
+use framing::{deframe::Deframer, mock::MockDeframer};
 use std::{
     sync::{
         Arc, Mutex,
@@ -12,27 +10,43 @@ use std::{
     thread,
     time::Duration,
 };
-use tracking::{Pass, Tracker, get_next_pass};
+use tracking::{Tracker, get_next_pass};
+mod time;
+
+#[cfg(feature = "time_mock")]
+use crate::time::MockClock as Clock;
+#[cfg(not(feature = "time_mock"))]
+use crate::time::SystemClock as Clock;
+
+use crate::time::TimeProvider;
 
 #[tokio::main]
 async fn main() {
+    #[cfg(feature = "time_mock")]
+    println!("Using mock time.");
+    #[cfg(not(feature = "time_mock"))]
+    println!("Using real system time.");
+
     let observer = tracking::Observer::new(-34.6, -58.4, 2.5);
     let elements = tracking::Elements::from_tle(
         Some("ISS (ZARYA)".to_owned()),
-        "1 25544U 98067A   25186.50618345  .00006730  00000+0  12412-3 0  9992".as_bytes(),
-        "2 25544  51.6343 216.2777 0002492 336.9059  23.1817 15.50384048518002".as_bytes(),
+        "1 25544U 98067A   25235.75642456  .00011222  00000+0  20339-3 0  9993".as_bytes(),
+        "2 25544  51.6355 332.1708 0003307 260.2831  99.7785 15.50129787525648".as_bytes(),
     )
     .unwrap();
 
     let mut next_pass = get_next_pass(
         &observer,
         &elements,
-        Utc::now(),
+        Clock::now(),
         Duration::from_secs_f64(3600.0 * 6.0),
     )
     .unwrap();
 
-    let mut timer = get_duration_until_pass(next_pass);
+    dbg!(&next_pass);
+
+    let mut timer = Duration::from_secs_f64(next_pass.start - Clock::now().timestamp() as f64);
+    println!("\nNext pass is in {:?} seconds.\n", timer.as_secs());
 
     let sleep = tokio::time::sleep(timer);
     tokio::pin!(sleep);
@@ -41,18 +55,27 @@ async fn main() {
         tokio::select! {
             _ = &mut sleep => {
 
+                println!("\nSTARTING PASS\n");
+
                 // SETUP TRACK
                 let stop = Arc::new(AtomicBool::new(false));
 
                 let (tx_samples, rx_samples) = mpsc::channel();
 
-                let demodulator = AfskDemodulator::new();
-                let deframer = HdlcDeframer::new();
-
+                let demodulator = ExampleDemod::new();
+                let deframer = MockDeframer::new();
                 let tracker = Tracker::new(&observer, elements.clone()).unwrap();
 
+                let observations = [0u8; 5]
+                    .iter()
+                    .map(|_| thread::sleep(Duration::from_secs(1)))
+                    .map(move |_| {
+                        tracker.track(Clock::now()).unwrap()
+                    });
+
+
                 let controller = Arc::new(Mutex::new(
-                    SerialAntennaController::new("/dev/pts/2", 9600).unwrap(),
+                    MockController
                 ));
                 // END SETUP TRACK
 
@@ -64,33 +87,27 @@ async fn main() {
                 let sdr_handle = thread::spawn(move || {
                     while !stop_clone.load(Ordering::Relaxed) {
                         tx_samples.send(0f64).unwrap();
+                        thread::sleep(Duration::from_millis(200));
                     }
                 });
 
                 let stop_clone = stop.clone();
-                let pass_end = next_pass.end;
                 let controller_clone = controller.clone();
 
                 let tracker_handle = thread::spawn(move || {
-                    loop {
-                        let now = chrono::Utc::now();
-
-                        if now.timestamp() as f64 > pass_end {
-                            // pass has ended
-                            stop_clone.store(true, Ordering::Relaxed);
-                            break;
-                        }
-
-                        let obs = tracker.track(now).unwrap();
+                    for obs in observations {
+                        dbg!(&obs);
 
                         controller_clone
                             .lock()
                             .unwrap()
                             .send(obs.azimuth, obs.elevation, "sat-name", 1000)
                             .unwrap();
-
-                        thread::sleep(Duration::from_millis(1000));
                     }
+
+                    println!("\nPass ended, stopping SDR and tracker.\n");
+
+                    stop_clone.store(true, Ordering::Relaxed);
                 });
 
                 for frame in frames {
@@ -102,25 +119,20 @@ async fn main() {
 
                 // END TRACKING
 
+                // Reschedule next pass
                 next_pass = get_next_pass(
                     &observer,
                     &elements,
-                    Utc::now(),
+                    Clock::now(),
                     Duration::from_secs_f64(3600.0 * 6.0),
                 )
                 .unwrap();
 
-                timer = get_duration_until_pass(next_pass);
+                timer = Duration::from_secs_f64(next_pass.start - Clock::now().timestamp() as f64);
+                println!("\nNext pass is in {:?} seconds.\n", timer.as_secs());
 
                 sleep.as_mut().reset(tokio::time::Instant::now() + timer);
             }
         }
     }
-}
-
-fn get_duration_until_pass(pass: Pass) -> Duration {
-    let now = chrono::Utc::now();
-    let now_timestamp = now.timestamp() as f64;
-
-    Duration::from_secs_f64(pass.start - now_timestamp)
 }
