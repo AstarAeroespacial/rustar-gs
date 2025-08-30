5ec61b95595075e9ff9bdc740ff747e82a029864
Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>
2025-08-30 13:35:40 -0300

    [cli] feat: basic tcp commands listener

--- ground-station/src/main.rs
+++ ground-station/src/main.rs
@@ -10,7 +10,11 @@ use std::{
     thread,
     time::Duration,
 };
-use tokio::task::spawn_blocking;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+    task::spawn_blocking,
+};
 use tracking::{Tracker, get_next_pass};
 mod time;
 
@@ -54,6 +58,30 @@ async fn main() {
 
     loop {
         tokio::select! {
+            maybe_conn = listener.accept() => {
+                if let Ok((mut socket, addr)) = maybe_conn {
+                    let mut buffer = [0; 2048];
+                    let n = socket.read(&mut buffer).await.unwrap();
+
+                    let request = String::from_utf8_lossy(&buffer[..n]);
+                    println!("Received from {}: {:?}", addr, request);
+
+                    match request.trim() {
+                        "GET_ELEMENTS" => socket
+                            .write_all(serde_json::to_string(&elements).unwrap().as_bytes())
+                            .await
+                            .unwrap(),
+                        "GET_OBSERVER" => socket
+                            .write_all(serde_json::to_string(&observer).unwrap().as_bytes())
+                            .await
+                            .unwrap(),
+                        "PING" => socket.write_all("PONG".as_bytes()).await.unwrap(),
+                        _ if request.starts_with("SET_OBSERVER") => todo!(),
+                        _ if request.starts_with("SET_ELEMENTS") => todo!(),
+                        _ => panic!(),
+                    }
+                }
+            }
             _ = &mut sleep => {
                 println!("\nSTARTING PASS\n");
 
