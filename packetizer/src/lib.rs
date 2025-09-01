cefc647a8ea171cbd8822958cad680645a74ccc4
alendavies <alendavies10@gmail.com>
2025-08-31 23:40:36 -0300

    feat: implement Packetizer trait for frame to packet conversion

--- packetizer/src/lib.rs
+++ packetizer/src/lib.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+pub mod packetizer;
+
+pub trait Packetizer<F, P> {
+    /// Creates an iterator that converts frames into packets.
+    fn packets<I>(&self, input: I) -> impl Iterator<Item = P>
+    where
+        I: Iterator<Item = F>;
 }
