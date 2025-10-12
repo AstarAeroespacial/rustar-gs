4d5a6547538695cf3cecf13533a54325bd9046c4
Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>
2025-10-12 15:51:00 -0300

    [sdr] chore: read samples return option

--- sdr/src/lib.rs
+++ sdr/src/lib.rs
@@ -2,7 +2,7 @@ use std::f64::consts::PI;
 
 pub trait Sdr {
     fn set_rx_frequency(&mut self, frequency: f64);
-    fn read_samples(&mut self) -> Vec<f64>;
+    fn read_samples(&mut self) -> Option<Vec<f64>>;
 }
 
 /// Mock SDR that generates a synthetic sine wave in baseband IQ
@@ -31,7 +31,7 @@ impl Sdr for MockSdr {
         self.freq = freq_hz;
     }
 
-    fn read_samples(&mut self) -> Vec<f64> {
+    fn read_samples(&mut self) -> Option<Vec<f64>> {
         let mut out = Vec::with_capacity(2 * self.block_size);
         let phase_inc = 2.0 * PI * self.freq / self.sample_rate;
 
@@ -45,7 +45,47 @@ impl Sdr for MockSdr {
 
         // println!("[SDR] Pushing samples");
 
-        out
+        Some(out)
+    }
+}
+
+pub struct ZmqMockSdr {
+    sub_sock: zmq::Socket,
+}
+
+impl ZmqMockSdr {
+    pub fn new(endpoint: String) -> Self {
+        let ctx = zmq::Context::new();
+        let sub_sock = ctx.socket(zmq::SUB).unwrap();
+        sub_sock.connect(&endpoint).unwrap();
+        sub_sock.set_subscribe(b"").unwrap();
+        sub_sock.set_rcvtimeo(1000).unwrap();
+
+        Self { sub_sock }
+    }
+}
+
+impl Sdr for ZmqMockSdr {
+    fn set_rx_frequency(&mut self, freq_hz: f64) {
+        println!("[ZMQ SDR] Setting frequency to {} Hz", freq_hz);
+    }
+
+    fn read_samples(&mut self) -> Option<Vec<f64>> {
+        if let Ok(msg) = self.sub_sock.recv_bytes(0) {
+            Some(msg.iter().map(|&b| b as f64).collect())
+        } else {
+            None
+        }
+    }
+}
+
+impl Sdr for Box<dyn Sdr + Send> {
+    fn set_rx_frequency(&mut self, frequency: f64) {
+        (**self).set_rx_frequency(frequency)
+    }
+
+    fn read_samples(&mut self) -> Option<Vec<f64>> {
+        (**self).read_samples()
     }
 }
 
@@ -68,12 +108,12 @@ pub async fn sdr_task(
             }
             Err(tokio::sync::mpsc::error::TryRecvError::Empty) => {
                 // no command right now, go generate samples
-                let samples = sdr.read_samples();
-                // dbg!(samples.len());
-                // NOTE: We'll have to be careful with this. The std channel sender is not
-                // blocking so it's okay to use here, but if we move to a _bounded_ channel,
-                // then we will have to consider making it async bc it blocks when full.
-                samples_tx.send(samples).unwrap(); // panicked here once
+                if let Some(samples) = sdr.read_samples() {
+                    // NOTE: We'll have to be careful with this. The std channel sender is not
+                    // blocking so it's okay to use here, but if we move to a _bounded_ channel,
+                    // then we will have to consider making it async bc it blocks when full.
+                    samples_tx.send(samples).unwrap(); // panicked here once
+                }
             }
             Err(tokio::sync::mpsc::error::TryRecvError::Disconnected) => break,
         }
